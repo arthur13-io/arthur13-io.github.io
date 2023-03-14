914abe268623e87960b399d4dc50c776dedf711b
huncho-hackz <davart776@gmail.com>
2023-03-14 21:58:31 +0000

    show value on cards

--- index.js
+++ index.js
@@ -9,22 +9,10 @@ modalClose.addEventListener('click', function(){
     modalContainer.classList.remove('modal-bg-active');
 });
 
-// function userInput('submit'){
-//     event.preventDefault();
-//     var payment = document.getElementById('payment').value;
-//     const database_api = 'http://localhost/phpmyadmin/index.php?route=/sql&db=cementogram&table=suppliers&pos=0';
-//     fetch(database_api, {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({ payment: payment })
-//       })
-//       .then(response => {
-//         console.log('Form data saved successfully');
-//       })
-//       .catch(error => {
-//         console.error('Error saving form data:', error);
-//       });
-//     }
-
+const payment = document.getElementById('paymentInput');
+const saveButton = document.getElementById('save');
+const paymentOutput = document.getElementById('paymentOutput');
+function changePayment(){
+    paymentOutput.innerHTML = `$${payment.value}`;
+}
+saveButton.addEventListener('click', changePayment);
