f51112e5b36e929dfbcd65dc5b6e47ed25840f8b
huncho-hackz <davart776@gmail.com>
2023-03-16 12:35:36 +0000

    showing inputs on cards

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Modal container
 var modalBtn = document.querySelector('.primary-btn');
 var modalContainer = document.querySelector('.modal-container');
 var modalClose = document.querySelector('.close-btn');
@@ -9,10 +10,26 @@ modalClose.addEventListener('click', function(){
     modalContainer.classList.remove('modal-bg-active');
 });
 
-const payment = document.getElementById('paymentInput');
+//Outputting the values on the various cards
 const saveButton = document.getElementById('save');
 const paymentOutput = document.getElementById('paymentOutput');
-function changePayment(){
-    paymentOutput.innerHTML = `$${payment.value}`;
+const supplierOutput = document.getElementById('supplierOutput');
+const amountOutput = document.getElementById('amountOutput');
+const quantityOutput = document.getElementById('quantityOutput');
+    
+function changePayment(e){
+    e.preventDefault(); //stop form from submitting
+    let inputs = {
+        payment: document.getElementById('paymentInput').value,
+        quantity: document.getElementById('quantity').value,
+        amount: document.getElementById('amount').value,
+        supply: document.getElementById('supply').value
+    }
+    paymentOutput.innerText = inputs.payment;
+    supplierOutput.innerText = inputs.supply;
+    amountOutput.innerText = inputs.amount;
+    quantityOutput.innerText = inputs.quantity;
 }
+
 saveButton.addEventListener('click', changePayment);
+
